b38a3a4a9b6646f5494bbd5a58fb2c796dff201d
Sajan Mishra <sjnmishra8@gmail.com>
2016-08-19 00:27:49 +0530

    finished chapter 5 problem 3

--- HigherOrderFunctions/HistoricalLifeExpectancy.js
+++ HigherOrderFunctions/HistoricalLifeExpectancy.js
@@ -9,10 +9,24 @@ var computeGroup = function (person) {
     return Math.ceil(person.died/100);
 };
 
-var groupBy = function(personArray, computeGroup) {
-    
-}
+var groupBy = function(personArray, f) {
+  var byCentury = {};
+  for (var i=0; i<personArray.length; i++) {
+  	var century = f(personArray[i]);
+    var age = personArray[i].died - personArray[i].born;
+    if(byCentury.hasOwnProperty(century))
+      byCentury[century].push(age);
+    else
+      byCentury[century] = [age];
+  }
+  return byCentury;
+};
 
+var ageSortedArray = groupBy(ancestry, computeGroup);
+for (var key in ageSortedArray) {
+  if(ageSortedArray.hasOwnProperty(key))
+    console.log(key+":"+" "+average(ageSortedArray[key]));
+}
 // → 16: 43.5
 //   17: 51.2
 //   18: 52.8
